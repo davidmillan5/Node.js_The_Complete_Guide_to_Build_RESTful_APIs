183526ddb6e10593a454be3f51672bc9231892b4
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-04-05 07:23:11 -0500

    Joi Package isnt working

--- 4_Building_RESTful_API_s_Using_Express/4_1_Express_Server/main.js
+++ 4_Building_RESTful_API_s_Using_Express/4_1_Express_Server/main.js
@@ -2,7 +2,19 @@
 
 const express = require('express'),
   app = express(),
-  PORT = process.env.PORT || 3000;
+  PORT = process.env.PORT || 3000,
+  Joi = require('joi');
+
+app.use(express.json()); // Adding a piece of middleware
+
+const courses = [
+  {
+    id: 1,
+    name: 'course 1',
+  },
+  { id: 2, name: 'course 2' },
+  { id: 3, name: 'course 3' },
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -12,8 +24,26 @@ app.get('/api/courses', (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.get('/api/courses/id:', (req, res) => {
-  res.send(req.params.id);
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send('The course with the given ID was not found');
+  res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.valid(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error);
+    return;
+  }
+  const course = { id: courses.length + 1, name: req.body.name };
+  courses.push(course);
+  res.send(course);
 });
 
 app.listen(PORT, () => {
